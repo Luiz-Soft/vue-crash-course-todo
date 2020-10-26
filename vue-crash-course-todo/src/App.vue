268a5691121f45585794989aabc5b8ee18e82c1e
Luiz França <62324134+Luiz-Soft@users.noreply.github.com>
2020-10-26 20:25:29 -0300

    project without http requests

---
+++ vue-crash-course-todo/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <Header />
+    <addTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Header from "./components/layout/header.vue";
+import Todos from "./components/todos.vue";
+import addTodo from "./components/addTodo.vue";
+
+export default {
+  name: "App",
+  components: {
+    Todos,
+    Header,
+    addTodo,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo one",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Todo two",
+          completed: false,
+        },
+        {
+          id: 3,
+          title: "Todo three",
+          completed: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+</style>
